bd9c9b1e1f840b9d1118e77f66988ab5b168c62f
drick <valick_rick@mail.ru>
2026-06-23 00:19:07 +0300

    Create Task

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorLib;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            Console.WriteLine("=== CalculatorLib Demo ===");
+
+            Console.WriteLine($"10 + 5 = {calculator.Add(10, 5)}");
+            Console.WriteLine($"10 - 5 = {calculator.Subtract(10, 5)}");
+            Console.WriteLine($"10 * 5 = {calculator.Multiply(10, 5)}");
+            Console.WriteLine($"10 / 5 = {calculator.Divide(10, 5)}");
+
+            Console.WriteLine("\nНажмите любую клавишу...");
+            Console.ReadKey();
+        }
+    }
+}
